c3326d437be9a16bdfaebbae905ec9f239bff0b9
tasakos-dev <tassosliot@protonmail.com>
2023-04-21 23:50:44 +0300

    make progress on income_by_country transformation

--- scripts/ETL/hash_map_matsi_matsi.py
+++ scripts/ETL/hash_map_matsi_matsi.py
@@ -1,10 +1,9 @@
-import pandas
 import parser
 import matsimatsi
 
 countries_file = "../../countries.csv"
 income_by_country = "../../Income by Country.xlsx"
-
+out = "./outputs/"
 matsi = {
     "Lao People's Democratic Republic": "Laos",
     "Bolivia (Plurinational State of)": "Bolivia",
@@ -14,6 +13,7 @@ matsi = {
     "Congo (Democratic Republic of the)": "Democratic Republic of the Congo",
     "Eswatini (Kingdom of)": "Swaziland",
     "Côte d'Ivoire": "Ivory Coast",
+    "CÃ´te d'Ivoire": "Ivory Coast",
     "Moldova (Republic of)": "Moldova",
     "Viet Nam": "Vietnam",
     "Micronesia (Federated States of)": "Micronesia",
@@ -29,19 +29,72 @@ matsi = {
     "Hong Kong; China (SAR)": "Hong Kong"
 }
 
+ignoring_list = [
+        "Organization for Economic Co-operation and Development",
+        "High human development",
+        "Medium human development",
+        "Developing Countries",
+        "Very high human development",
+        "Least Developed Countries",
+        "Arab States",
+        "Human Development",
+        "Europe and Central Asia",
+        "Low human development",
+        "East Asia and the Pacific",
+        "Regions",
+        "Small Island Developing States",
+        "World",
+        'South Asia', 'Sub-Saharan Africa', 'Latin America and the Caribbean'
+    ]
 
-def hashmap_matsi_matsi():
+def matsi_names(cname):
+    if cname is not None and cname not in ignoring_list:
+            if cname in matsi:
+                return matsi[cname]
+            else:
+                return cname
 
+# years = [year for year in range(1990, 2019)]      
+def all_str(lst):
+    for elem in lst:
+        if elem is not None and not isinstance(elem, str):
+            return False
+    
+    return True
+     
+shits = dict()
 
+for shit in parser.get_xlsx_sheets(income_by_country):
+    countries = dict()
+    shits[shit] = countries
+    content = parser.load_xlsx_sheet(income_by_country, shit)
+    headers = list(content.keys())
+    for j, cname in enumerate(content['Country']):
+         vals = []
+         countries[matsi_names(cname)] = vals
+         for i in range(len(headers)):
+              year = headers[i]
+              if (type(year) == int):
+                val = content[year][j]
+                vals.append(val)
+    
 
-	the_countries_matsi = []
+for shit in parser.get_xlsx_sheets(income_by_country):
+    lst = []
+    countries = shits[shit]
+    for key, values in countries.items():
+        if all_str(values):
+            lst.append(key)
+    for key in lst:
+        del countries[key]
+    print(len(countries['South Sudan']))
+    break
+    
+# test1 = set(list(shits['Income Index'].keys()))
+# the_other_matsi = set(parser.load_csv(countries_file)["Display_Name"])
 
-	try:
-	    the_countries_matsi = parser.load_csv(countries_file)["Display_Name"]
-	    
-	except UnicodeDecodeError:
-	    pass  # ignore the error and continue with the code
+# print(matsimatsi.matsi_matsi(test1, the_other_matsi))
 
-	GDP_Income_by_country_matsi = parser.load_xlsx_sheet(income_by_country, "GDP total")["Country"]
-
-hashmap_matsi_matsi()	
+              
+                   
+    

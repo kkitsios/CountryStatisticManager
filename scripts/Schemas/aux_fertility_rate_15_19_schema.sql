01dc6efe61bcf444d16f8dddb92f13865292b815
tasakos-dev <tassosliot@protonmail.com>
2023-05-08 23:23:16 +0300

    Revert

--- scripts/Schemas/aux_fertility_rate_15_19_schema.sql
+++ scripts/Schemas/aux_fertility_rate_15_19_schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE aux_fertility_rate_15_19 (
   country_code VARCHAR(3) NOT NULL,
   country_name VARCHAR(255) NOT NULL,
   year INT NOT NULL,
-  value FLOAT,
+  fertility_rate_15_19 FLOAT,
   PRIMARY KEY (country_code, year)
 );
--- scripts/Schemas/aux_fertility_rate_25_29_schema.sql
+++ scripts/Schemas/aux_fertility_rate_25_29_schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE aux_fertility_rate_25_29 (
   country_code VARCHAR(3) NOT NULL,
   country_name VARCHAR(255) NOT NULL,
   year INT NOT NULL,
-  value FLOAT,
+  fertility_rate_25_29 FLOAT,
   PRIMARY KEY (country_code, year)
 );

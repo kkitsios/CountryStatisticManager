a5484ba23eef13da7751e2988ba9ab763bf0c6e5
Konstantinos Kitsios <costaskitsios@protonmail.com>
2023-05-12 01:45:12 +0300

    Done aux_loading.

--- scripts/Schemas/aux_mortality_rate_1to4_male_schema.sql
+++ scripts/Schemas/aux_mortality_rate_1to4_male_schema.sql
@@ -1,7 +1,7 @@
-CREATE TABLE aux_mortality_rate_1to4 (
+CREATE TABLE aux_mortality_rate_1to4_male (
   country_code VARCHAR(3) NOT NULL,
   country_name VARCHAR(255) NOT NULL,
   year INT NOT NULL,
-  mortality_rate_1to4 FLOAT,
+  mortality_rate_1to4_male FLOAT,
   PRIMARY KEY (country_code, year)
 );

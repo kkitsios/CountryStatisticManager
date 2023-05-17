4163d8665786540063490cd19ef6b4b0552b0abc
tasakos-dev <tassosliot@protonmail.com>
2023-05-18 01:32:05 +0300

    Create controllers

--- MatsoData/src/main/java/csm/controller/PlotController.java
+++ MatsoData/src/main/java/csm/controller/PlotController.java
@@ -1,5 +1,58 @@
 package csm.controller;
 
-public class PlotController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.stage.Stage;
+
+@Controller
+public class PlotController implements Initializable{
+	
+	@FXML
+	private BarChart<String, Double> barChart;
+	
+	@FXML
+	private LineChart<String, Double> lineChart;
+	
+	@FXML
+	private PieChart pieChart;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	
+	@FXML
+	public void back() {
+	    FXMLLoader fxmlLoader = new FXMLLoader();
+	    fxmlLoader.setLocation(getClass().getClassLoader().getResource("Statistics.fxml"));
+	    Scene scene;
+	    try {
+	      Stage stage = new Stage();
+	      scene = new Scene(fxmlLoader.load());
+	      stage.setScene(scene);
+//	      stage.setUserData(trn);
+
+	      stage.setResizable(false);
+	      stage.show();
+	      ((Stage) pieChart.getScene().getWindow()).close();
+			
+	    } catch (Exception e) {
+			 e.printStackTrace();
+		}
+	    
+		
+	}
 
 }
--- MatsoData/src/main/java/csm/controller/StatisticController.java
+++ MatsoData/src/main/java/csm/controller/StatisticController.java
@@ -1,5 +1,61 @@
 package csm.controller;
 
-public class StatisticController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.springframework.stereotype.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+@Controller
+public class StatisticController implements Initializable {
+	
+	@FXML
+	private ComboBox<String> country;
+	
+	@FXML
+	private ComboBox<String> year;
+	
+	@FXML
+	private ComboBox<String> stat1;
+	
+	@FXML
+	private ComboBox<String> stat2;
+	
+	@FXML
+	private Button showBtn;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+	
+	@FXML
+	public void createPlots() {
+	    FXMLLoader fxmlLoader = new FXMLLoader();
+	    fxmlLoader.setLocation(getClass().getClassLoader().getResource("Plots.fxml"));
+	    Scene scene;
+	    try {
+	      Stage stage = new Stage();
+	      scene = new Scene(fxmlLoader.load());
+	      stage.setScene(scene);
+//	      stage.setUserData(trn);
+
+	      stage.setResizable(false);
+	      stage.show();
+	      ((Stage) country.getScene().getWindow()).close();
+			
+	    } catch (Exception e) {
+			 e.printStackTrace();
+		}
+	}
+	
 
 }

9754569901ddf84d2d51229aabd8b205f0dfd9ee
Konstantinos Kitsios <costaskitsios@protonmail.com>
2023-05-17 23:58:56 +0300

    Pare

--- scripts/Load/load_international_data.sql
+++ scripts/Load/load_international_data.sql
@@ -1,36 +1,32 @@
-USE matso_data;
-CREATE TABLE international_dataset AS 
+CREATE TABLE international_data AS
 SELECT *
-FROM aux_5yr_age_midyear_population
-NATURAL JOIN aux_5yr_age_midyear_population_female
-NATURAL JOIN aux_5yr_age_midyear_population_male
-NATURAL JOIN aux_countries_area
-NATURAL JOIN aux_crude_birth_rate
-NATURAL JOIN aux_crude_death_rate
-NATURAL JOIN aux_domestic_credits
-NATURAL JOIN aux_fertility_rate_15_19
-NATURAL JOIN aux_fertility_rate_20_24
-NATURAL JOIN aux_fertility_rate_25_29
-NATURAL JOIN aux_fertility_rate_30_34
-NATURAL JOIN aux_fertility_rate_35_39
-NATURAL JOIN aux_fertility_rate_40_44
-NATURAL JOIN aux_fertility_rate_45_49
-NATURAL JOIN aux_gross_reproduction_rate
-NATURAL JOIN aux_growth_rate
-NATURAL JOIN aux_infant_mortality
-NATURAL JOIN aux_infant_mortality_female
-NATURAL JOIN aux_infant_mortality_male
-NATURAL JOIN aux_life_expectancy
-NATURAL JOIN aux_life_expectancy_female
-NATURAL JOIN aux_life_expectancy_male
-NATURAL JOIN aux_midyear_population
-NATURAL JOIN aux_midyear_population_age_sex
-NATURAL JOIN aux_mortality_rate_1to4
-NATURAL JOIN aux_mortality_rate_1to4_female
-NATURAL JOIN aux_mortality_rate_under5
-NATURAL JOIN aux_mortality_rate_under5_female
-NATURAL JOIN aux_mortality_rate_under5_male
-NATURAL JOIN aux_net_migration
-NATURAL JOIN aux_rate_natural_increase
-NATURAL JOIN aux_sex_ratio_at_birth
-NATURAL JOIN aux_total_fertility_rate;
+FROM fertility_rate_15_19
+NATURAL JOIN fertility_rate_20_24
+NATURAL JOIN fertility_rate_25_29
+NATURAL JOIN fertility_rate_30_34
+NATURAL JOIN fertility_rate_35_39
+NATURAL JOIN fertility_rate_40_44
+NATURAL JOIN fertility_rate_44_49
+NATURAL JOIN crude_birth_rate
+NATURAL JOIN crude_death_rate
+NATURAL JOIN gross_reproduction_rate
+NATURAL JOIN growth_rate
+NATURAL JOIN infant_mortality
+NATURAL JOIN infant_mortality_male
+NATURAL JOIN infant_mortality_female
+NATURAL JOIN life_expectancy
+NATURAL JOIN life_expectancy_male
+NATURAL JOIN life_expectancy_female
+NATURAL JOIN midyear_population
+NATURAL JOIN mortality_rate_1to4
+NATURAL JOIN mortality_rate_1to4_male
+NATURAL JOIN mortality_rate_1to4_female
+NATURAL JOIN mortality_rate_under5
+NATURAL JOIN mortality_rate_under5_male
+NATURAL JOIN mortality_rate_under5_female
+NATURAL JOIN net_migration
+NATURAL JOIN sex_ratio_at_birth
+NATURAL JOIN total_fertility_rate
+NATURAL JOIN rate_natural_increase;
+alter table international_data add constraint primary key (country_code, year); 
+alter table international_data add constraint foreign key (Iso_code) references countries (Iso_code);

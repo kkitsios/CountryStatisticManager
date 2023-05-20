2bf75bc5910e64100b5254b86e1e555ec0a7d1a9
Konstantinos Kitsios <costaskitsios@protonmail.com>
2023-05-20 22:11:53 +0300

    Added some more queries

---
+++ MatsoData/src/main/java/csm/dao/CountryAreaDAOImpl.java
@@ -0,0 +1,56 @@
+package csm.dao;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import csm.entity.InternationalData;
+import csm.entity.InternationalDataPK;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import csm.entity.CountryArea;
+
+@Repository("country_area")
+public class CountryAreaDAOImpl implements MetricsDAO {
+    private final EntityManager entityManager;
+
+    public CountryAreaDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<Double> findMetricByCountryBetween(String stat, List<String> countries,int startYear,  int endYear) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Double> query = builder.createQuery(Double.class);
+        Root<CountryArea> root = query.from(CountryArea.class);
+
+
+        List<Double> results = new ArrayList<>();
+
+
+        query.select(root.get(stat));
+        query.where(root.get("countryName").in(countries)
+        );
+
+        List<Double> queryResults = entityManager.createQuery(query).getResultList();
+        results.addAll(queryResults);
+
+
+        return results;
+    }
+
+}
